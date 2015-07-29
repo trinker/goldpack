9cac51cf7460772482884a5efd59ddea12713d60
Bryan Goodrich <goodrich.bryan@gmail.com>
2015-07-29 15:38:20 -0700

    Restructure what function
    Redesign the what logic. Last line can assume x is neither a data frame,
    a list, a character vector, nor an environment. To avoid inappropriately
    calling any other non-vector options (like what?) a "vector" replace
    "else" with "else if" for consistency with previous form.

--- R/new_data.R
+++ R/new_data.R
@@ -135,19 +135,15 @@ roxdat <- function(dat, name, file = "", append = FALSE) {
 is.enviroment <- function(x) class(x) == "environment"
 
 what <- function(x) {
-    if (is.data.frame(x)) {
-        return("data frame")
-    }
-    if (is.list(x) & !is.data.frame(x)) {
-        return("list")
-    }
-    if (class(x) == "character") {
-        return("character vector")
-    }
-    if (is.vector(x)) {
-        return("vector")
-    }
-    if (is.environment(x)) {
-        return("environment")
+    what <- function(x) {
+        if (is.data.frame(x)) {
+            return ("data frame")
+        } else if (is.list(x)) {
+            return ("list")
+        } else if (class(x) == "character") {
+            return ("character vector")
+        } else if (is.environment(x)) {
+            return ("environment")
+        } else return ("vector")
     }
 }
